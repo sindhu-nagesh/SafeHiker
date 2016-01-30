fe8be5213fe6f7bf50b6863f4798ffe88b5551b5
Vidya Sindhu Nagesh <sindhu.n@outlook.com>
2016-01-29 21:23:49 -0800

    Initial commit

---
+++ SafeHiker/Scripts/myScripts.js
@@ -0,0 +1,14 @@
+﻿function showpage2() {
+    document.getElementById('pagetwo').style.display = '';
+    document.getElementById('pageone').style.display = 'none';
+}
+
+function showpage1() {
+    document.getElementById('pagetwo').style.display = 'none';
+    document.getElementById('pageone').style.display = '';
+}
+
+$(document).ready(function () {
+    // Use allowtimes for adding more granularity
+    $(".datetime").datetimepicker();
+});
---
+++ SafeHiker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SafeHiker.Startup))]
+namespace SafeHiker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
