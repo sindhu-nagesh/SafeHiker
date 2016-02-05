50fc7330323af7bd99aea63c47fe3b78b608d1ea
Vidya Sindhu Nagesh <sindhu.n@outlook.com>
2016-02-05 00:05:47 -0800

    Added my hikes controller

--- SafeHikerWebsite/ViewModels/UserViewModel.js
+++ SafeHikerWebsite/ViewModels/UserViewModel.js
@@ -1,8 +1,17 @@
 ﻿var HasUserData = ko.observable(false);
-var userData = {
+var UserData = {
     name: ko.observable(""),
     emergencyEmail1: ko.observable(""),
     emergencyEmail2: ko.observable("")
+};
+
+var HikeData = {
+    hikeName: ko.observable(""),
+    emergencyEmail1: ko.observable(""),
+    emergencyEmail2: ko.observable(""),
+    startDateAndTime: ko.observable(""),
+    endDateAndTime: ko.observable(""),
+    notifyDateAndTime: ko.observable("")
 };
 
 var pages = ['pageone', 'pagetwo', 'pagethree', 'pagefour', 'pagefive'];
@@ -11,6 +20,7 @@ $(document).ready(function () {
     //    Use allow times for adding more granularity
     $(".datetime").datetimepicker();
     showpage(pages[0]);
+    ko.applyBindings();
 
     var userEmail = document.getElementById("userEmail").defaultValue;
     $.ajax({
@@ -23,7 +33,7 @@ $(document).ready(function () {
                 HasUserData(false);
                 showpage(pages[1]);
             } else {
-                userData.name(data.Name);
+                UserData.name(data.Name);
                 HasUserData(true);
                 showpage(pages[2]);
             }
@@ -40,4 +50,21 @@ function showpage(pageId) {
             document.getElementById(pages[i]).style.display = 'none';
         }
     }
+}
+
+function clearHikeData() {
+    HikeData.hikeName("");
+    HikeData.emergencyEmail1("");
+    HikeData.emergencyEmail2("");
+    HikeData.startDateAndTime("");
+    HikeData.endDateAndTime("");
+    HikeData.notifyDateAndTime("");
+    showpage('pagefive');
+}
+
+function clearUserData() {
+    UserData.name("");
+    UserData.emergencyEmail1("");
+    UserData.emergencyEmail2("");
+    showpage('pagefour');
 }
