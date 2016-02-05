7ce689aeb2a1dea7cbd63cc1d76498284cbd2ca8
Vidya Sindhu Nagesh <sindhu.n@outlook.com>
2016-02-04 21:59:30 -0800

    Commiting missed files

---
+++ SafeHikerService/Utilities/StorageUtitlity.cs
@@ -0,0 +1,21 @@
+﻿using SafeHikerService.Models;
+using SafeHikerService.TableEntities;
+
+namespace Common
+{
+    public class StorageUtitlity
+    {
+        public HikeDataModel ToHikeDataModel(HikeDataEntity entity)
+        {
+            return new HikeDataModel
+            {
+                HikeName = entity.HikeName,
+                EmergencyEmail1 = entity.EmergencyEmail1,
+                EmergencyEmail2 = entity.EmergencyEmail2,
+                StartDateAndTime = entity.StartDateAndTime,
+                EndDateAndTime = entity.EndDateAndTime,
+                NotifyEmergencyContactDateAndTime = entity.NotifyEmergencyContactDateAndTime
+            };
+        }
+    }
+}
